a29ab3d3b29f99c2f7fbf0a5932035246e7b8e51
Denis <denis@localhost.localdomain>
2024-12-16 14:07:05 +0100

    Liste de fruits

---
+++ Exercice1Train.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<String> fruits = ["Pomme", "Ananas", "Banane", "Fraise", "Orange"];
+
+  fruits.add("Mangue");
+
+  print(fruits);
+
+  fruits.removeAt(2);
+
+  print(fruits);
+}
